f4c08c03ff39c9d5cb26b222f427cb64399fcee5
Christian Rivera <christian.rivera@agilesolutions.com>
2016-03-12 20:06:19 -0600

    Adding character class, however it doesnt recognize string

---
+++ Character.cpp
@@ -0,0 +1,11 @@
+#include "Character.h"
+
+Character::Character(int attack, int life, int x, int y, string type)
+{
+    //ctor
+}
+
+Character::~Character()
+{
+    //dtor
+}
---
+++ Character.h
@@ -0,0 +1,17 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+#include <string>
+
+
+class Character
+{
+    public:
+        int attack,life,x,y;
+        string type;
+        Character(int attack, int life, int x, int y, string type);
+        virtual ~Character();
+    protected:
+    private:
+};
+
+#endif // CHARACTER_H
