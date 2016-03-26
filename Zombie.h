c052f660d0f49c42c58a045d0087bd76b6af591c
Christian Rivera <christian.rivera@agilesolutions.com>
2016-03-26 11:07:15 -0600

    just created zombie class from character

---
+++ Zombie.h
@@ -0,0 +1,16 @@
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
+
+#include <.h>
+
+
+class Zombie : Character
+{
+    public:
+        Zombie();
+        virtual ~Zombie();
+    protected:
+    private:
+};
+
+#endif // ZOMBIE_H
