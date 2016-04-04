6a5ce93eb47edadd5ad75adcbc08849ae21f43f2
Lourdes Zamora <lourdes.zamora@unitec.edu>
2016-04-04 02:58:13 -0600

    no message

---
+++ Menu.h
@@ -0,0 +1,20 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_native_dialog.h>
+
+class Menu
+{
+    public:
+        Menu();
+        void addBackground(ALLEGRO_BITMAP *background, int selected);
+        virtual ~Menu();
+    protected:
+    private:
+};
+
+#endif // MENU_H
