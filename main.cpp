774cc51a68d5726d235303e8a72d093655e6d6d1
Christian Rivera <christian.rivera@agilesolutions.com>
2016-04-04 02:12:44 -0600

    changing main to menu

--- main.cpp
+++ main.cpp
@@ -1,110 +1,137 @@
-using namespace std;
-
 #include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_native_dialog.h>
 
 #include "Menu.h"
-Menu* m;
-ALLEGRO_DISPLAY *display;
-ALLEGRO_EVENT_QUEUE *event_queue;
-ALLEGRO_TIMER *timer;
-ALLEGRO_BITMAP *menu_start,*menu_exit,*menu_tutorial,*menu_selected;
 
-enum LEVEL { MENU, LEVEL1, LEVEL2 };
-LEVEL level= MENU;
 
+
+int allegro_init(){
+
+    if(!al_init())
+    {
+        al_show_native_message_box(NULL, "Error!", "Allegro has failed to initialize.", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
+        return -1;
+    }
+
+    if(!al_init_image_addon())
+    {
+        al_show_native_message_box(NULL, "Error!", "Image addon has failed to initialize.", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
+        return -1;
+    }
+
+    if(!al_install_audio()){
+      fprintf(stderr, "failed to initialize audio!\n");
+      return -1;
+    }
+
+    if(!al_init_acodec_addon()){
+      fprintf(stderr, "failed to initialize audio codecs!\n");
+      return -1;
+    }
+
+    if (!al_reserve_samples(1)){
+        fprintf(stderr, "failed to reserve samples!\n");
+        return -1;
+    }
+
+    return 1;
+
+}
+Menu *m= new Menu();
 int main()
 {
+    ALLEGRO_DISPLAY *Screen = NULL;
+    ALLEGRO_EVENT_QUEUE *EventQueue = NULL;
+    ALLEGRO_EVENT Event;
+    ALLEGRO_BITMAP *Background=NULL;
+    ALLEGRO_SAMPLE *sample=NULL;
 
-
-    int redraw=1;
-    al_init();
-    al_init_image_addon();
-    al_install_keyboard();
-
-    menu_exit=al_load_bitmap("menuexit.png");
-    menu_start=al_load_bitmap("menustart.png");
-    menu_tutorial=al_load_bitmap("menututorial.png");
-
-    display = al_create_display(1080, 680);
-
-    event_queue = al_create_event_queue();
-
-    timer = al_create_timer(1.0 / 60);
-
-
-    al_register_event_source(event_queue, al_get_display_event_source(display));
-    al_register_event_source(event_queue, al_get_timer_event_source(timer));
-    al_register_event_source(event_queue, al_get_keyboard_event_source());
-
-    al_start_timer(timer);
     int select=0;
-    ALLEGRO_EVENT event;
-    ALLEGRO_TIMEOUT timeout;
-    al_init_timeout(&timeout, 0.06);
-    while (1)
-    {
+    bool Exit = false;
+
+    //inicializar allegro
+
+    allegro_init();
 
 
-        bool get_event = al_wait_for_event_until(event_queue, &event, &timeout);
+    sample = al_load_sample("music.ogg");
+    if(sample==NULL){
+        printf( "Error al inicializar la musica!\n" );
+    }
 
-        if(get_event && event.type == ALLEGRO_EVENT_KEY_DOWN) ///look for keyboard events
+    Screen = al_create_display(1080, 680);
+        if(Screen == NULL)
         {
-            if (event.keyboard.keycode == ALLEGRO_KEY_UP){
+            al_show_native_message_box(Screen, "Error!", "Failed to create the display.", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
+            return -1;
+        }
+
+    EventQueue = al_create_event_queue();
+        if(EventQueue == NULL)
+        {
+            al_show_native_message_box(Screen, "Error!", "Failed to create the event queue.", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
+            return -1;
+        }
+
+    if(!al_install_keyboard())
+    {
+        al_show_native_message_box(NULL, "Error!", "Failed to install keyboard.", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
+        return -1;
+    }
+
+    al_register_event_source(EventQueue, al_get_display_event_source(Screen));
+    al_register_event_source(EventQueue, al_get_keyboard_event_source());
+
+
+
+
+    al_play_sample(sample, 1.0, 0.0,1.0,ALLEGRO_PLAYMODE_LOOP,NULL);
+
+   // Background=al_load_bitmap("menustart.png");
+    while(Exit == false)
+    {
+  /*      if(!m->addBackground(Background,select)){
+            al_show_native_message_box(NULL, "Error!", "Error al seleccionar el background.", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
+            return -1;
+        }*/
+
+        m->addBackground(Background,select);
+
+
+        al_flip_display();
+
+        al_wait_for_event(EventQueue, &Event);
+
+        if(Event.type == ALLEGRO_EVENT_KEY_DOWN)
+        {
+            if (Event.keyboard.keycode == ALLEGRO_KEY_UP){
                 select--;
 
             }
 
-            if (event.keyboard.keycode == ALLEGRO_KEY_DOWN){
+            if (Event.keyboard.keycode == ALLEGRO_KEY_DOWN){
                 select++;
             }
-
         }
-
         if (select>2)
             select=0;
         if (select<0)
             select=2;
 
-        if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-            break;
-
-
-            switch(select){
-            case 0:
-                menu_selected=menu_start;
-
-
-            break;
-            case 1:
-                menu_selected=menu_tutorial;
-
-            break;
-            case 2:
-                menu_selected=menu_exit;
-            break;
-
-
-
+        if(Event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+        {
+            Exit = true;
         }
-        al_draw_bitmap(menu_selected, 0, 0, 0);
-        al_flip_display();
-
-
-
-
-
-
     }
 
-    al_destroy_display(display);
-    al_destroy_event_queue(event_queue);
-    al_destroy_timer(timer);
-    al_destroy_bitmap(menu_selected);
-    al_destroy_bitmap(menu_exit);
-    al_destroy_bitmap(menu_start);
-    al_destroy_bitmap(menu_tutorial);
+    al_destroy_bitmap(Background);
+
+    al_destroy_event_queue(EventQueue);
+    al_destroy_display(Screen);
 
     return 0;
 }
-
-
